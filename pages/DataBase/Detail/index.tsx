4f96aee3e33027f64630d5767932addfc179a9b2
kursaderduhan <kursaderduhan42@gmail.com>
2022-09-26 02:45:18 +0300

    feat: added database detail page

---
+++ pages/DataBase/Detail/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+  VStack,
+  Flex,
+  Box,
+  Text,
+  HStack,
+  Container,
+  IconButton
+} from '@chakra-ui/react'
+import Link from 'next/link'
+import { ChevronLeftIcon } from '@chakra-ui/icons'
+import Layout from '@components/Layout/Layout'
+export const Detail = () => {
+  return (
+    <Layout>
+      <VStack
+        h={'170px'}
+        bg={'#E5EDF4'}
+        alignSelf={'flex-end'}
+        alignItems={'flex-start'}
+        justifyContent={'flex-end'}
+        gap={5}
+        py={10}
+      >
+        <Container maxW={'1200px'}>
+          <HStack>
+            <Link href={'/DataBase'}>
+              <IconButton
+                aria-label='back'
+                icon={<ChevronLeftIcon w={'24px'} h={'24px'} />}
+                size={'md'} bg={"transparent"} _hover={{bg:"transparent", opacity:0.8}}
+              ></IconButton>
+            </Link>
+            <Text fontSize={'15px'} fontWeight={500} color={'#333333'}>
+              724Desk Bilgi Bankası
+            </Text>
+          </HStack>
+        </Container>
+      </VStack>
+    </Layout>
+  )
+}
+
+export default Detail
